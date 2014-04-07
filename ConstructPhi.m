4f1f81f9749e232afb09c0ab3e429c214b145593
Sylvain Lobry <sylvain.lobry@telecom-paristech.fr>
2014-04-07 10:35:04 +0200

    Added a function to construct Phi from an image.
    	  * ConstructPhi.m: Here.

---
+++ ConstructPhi.m
@@ -0,0 +1,32 @@
+function [ Phi, LabelQuantification ] = ConstructPhi( Input )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+    VMax = max(max(Input));
+    
+    i = 1;
+    curVal = 0;
+    while (curVal < 1000)
+        LabelQuantification(i) = curVal;
+        curVal = curVal + 30;
+        i = i + 1;
+    end
+    while (curVal < VMax + 100)
+        LabelQuantification(i) = curVal;
+        curVal = curVal + 100;
+        i = i + 1;
+    end
+
+    [Height, Width] = size(Input);
+    [~,labelsSize] = size(LabelQuantification);
+    Phi = zeros(Height, Width, labelsSize);
+    
+    for(i=1:labelsSize)
+        [r, v] = find(Input >= LabelQuantification(i)); %FIX ME: Step to speed up!
+        for (j=1:size(r, 1))
+            Phi(r(j), v(j), i) = 1;
+        end
+    end
+    
+end
+
